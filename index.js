dbe1d69dd495076efaff4bf01a42ccc1ed9a5a7c
Lucy <lucy.d.bullen@gmail.com>
2022-05-18 20:50:56 -0600

    commit tree d6dc5b13537fdd1213a3dd22d3941f0ec6d1059d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const employee = {
+  name: 'Lucy',
+  streetAddress: '967 Froggy Village'
+}; 
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {...employee,
+     [key]:value};
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+  const employeeClone = Object.assign({}, employee);
+  delete employeeClone[key];
+  return employeeClone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
